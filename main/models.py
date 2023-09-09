1ebbfa135f437c74b66887b225a9181983b35612
fahmi-ramadhan <fahmi122004@gmail.com>
2023-09-09 18:55:02 +0700

    Create main app

---
+++ main/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+from django.db import models
+
+class Book(models.Model):
+    name = models.CharField(max_length=255)
+    category = models.CharField(max_length=255)
+    amount = models.IntegerField()
+    description = models.TextField()
---
+++ main/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+def show_main(request):
+    context = {
+        'name': 'Operating System Concepts',
+        'category': 'Computer Science',
+        'amount': '10',
+        'description': 'Operating System Concepts is a book written by Abraham Silberschatz, Peter B. Galvin, and Greg Gagne. It is an informative guide to operating systems with an overview of all the major aspects. The book deals with topics like process, operating systems and their functioning, and design. It also looks at special-purpose systems, storage management, security, distributed systems and memory.'
+    }
+
+    return render(request, "main.html", context)
