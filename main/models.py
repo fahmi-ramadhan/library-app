b8f7d5b5d651f6c50a2fdb140331a4f93962de11
fahmi-ramadhan <fahmi122004@gmail.com>
2023-09-09 19:21:22 +0700

    Update models.py

--- main/models.py
+++ main/models.py
@@ -1,7 +1,5 @@
 from django.db import models
 
-from django.db import models
-
 class Book(models.Model):
     name = models.CharField(max_length=255)
     category = models.CharField(max_length=255)
