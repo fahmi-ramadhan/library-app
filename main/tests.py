de228ec015f2c1b5f7044994c9874704a836868d
fahmi-ramadhan <fahmi122004@gmail.com>
2023-09-12 19:10:51 +0700

    Update `tests.py`

--- main/tests.py
+++ main/tests.py
@@ -17,10 +17,10 @@ class itemTest(TestCase):
             author="Abraham Silberschatz, Peter B. Galvin, and Greg Gagne",
             category="Computer Science",
             amount=10,
-            description="Operating System Concepts is a book written by Abraham Silberschatz, Peter B. Galvin, and Greg Gagne. It is an informative guide to operating systems with an overview of all the major aspects. The book deals with topics like process, operating systems and their functioning, and design. It also looks at special-purpose systems, storage management, security, distributed systems and memory."
+            description="Operating System Concepts book is an informative guide to operating systems with an overview of all the major aspects. The book deals with topics like computer process, operating systems and their functioning, and design. It also looks at special-purpose systems, storage management, security, distributed systems and memory."
         )
         self.assertEqual(item.name, "Operating System Concepts")
         self.assertEqual(item.author, "Abraham Silberschatz, Peter B. Galvin, and Greg Gagne")
         self.assertEqual(item.category, "Computer Science")
         self.assertEqual(item.amount, 10)
-        self.assertEqual(item.description, "Operating System Concepts is a book written by Abraham Silberschatz, Peter B. Galvin, and Greg Gagne. It is an informative guide to operating systems with an overview of all the major aspects. The book deals with topics like process, operating systems and their functioning, and design. It also looks at special-purpose systems, storage management, security, distributed systems and memory.")
+        self.assertEqual(item.description, "Operating System Concepts book is an informative guide to operating systems with an overview of all the major aspects. The book deals with topics like computer process, operating systems and their functioning, and design. It also looks at special-purpose systems, storage management, security, distributed systems and memory.")
