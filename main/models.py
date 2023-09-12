c35c134501d66e05fa13eaa14fa4f4d604de2297
fahmi-ramadhan <fahmi122004@gmail.com>
2023-09-12 16:10:20 +0700

    Add "author" attribute

--- main/models.py
+++ main/models.py
@@ -1,7 +1,8 @@
 from django.db import models
 
-class Book(models.Model):
+class Item(models.Model):
     name = models.CharField(max_length=255)
+    author = models.CharField(max_length=255)
     category = models.CharField(max_length=255)
     amount = models.IntegerField()
     description = models.TextField()
