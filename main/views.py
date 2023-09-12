683798c86d78257955107a3087f488b38eacc713
fahmi-ramadhan <fahmi122004@gmail.com>
2023-09-12 18:59:51 +0700

    Update `views.py`

--- main/views.py
+++ main/views.py
@@ -2,13 +2,14 @@ from django.shortcuts import render
 
 def show_main(request):
     context = {
+        'app_name': 'Library Management System',
         'student_name': 'Fahmi Ramadhan',
         'class': 'PBP A',
         'name': 'Operating System Concepts',
         'author': 'Abraham Silberschatz, Peter B. Galvin, and Greg Gagne',
         'category': 'Computer Science',
         'amount': '10',
-        'description': 'Operating System Concepts is a book written by Abraham Silberschatz, Peter B. Galvin, and Greg Gagne. It is an informative guide to operating systems with an overview of all the major aspects. The book deals with topics like process, operating systems and their functioning, and design. It also looks at special-purpose systems, storage management, security, distributed systems and memory.'
+        'description': 'Operating System Concepts book is an informative guide to operating systems with an overview of all the major aspects. The book deals with topics like computer process, operating systems and their functioning, and design. It also looks at special-purpose systems, storage management, security, distributed systems and memory.'
     }
 
     return render(request, "main.html", context)
