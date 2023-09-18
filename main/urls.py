c615126ed3455ff967dd338807e897ce97956d67
fahmi-ramadhan <fahmi122004@gmail.com>
2023-09-18 20:40:00 +0700

    Update tugas 3

--- main/urls.py
+++ main/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from main.views import show_main
+from main.views import show_main, add_book
 
 app_name = 'main'
 
 urlpatterns = [
     path('', show_main, name='show_main'),
+    path('add_book', add_book, name='add_book'),
 ]
--- main/views.py
+++ main/views.py
@@ -1,15 +1,27 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from main.forms import ItemForm
+from django.urls import reverse
+from main.models import Item
 
 def show_main(request):
+    books = Item.objects.all()
+
     context = {
         'app_name': 'Library Management System',
         'student_name': 'Fahmi Ramadhan',
         'class': 'PBP A',
-        'name': 'Operating System Concepts',
-        'author': 'Abraham Silberschatz, Peter B. Galvin, and Greg Gagne',
-        'category': 'Computer Science',
-        'amount': '10',
-        'description': 'Operating System Concepts book is an informative guide to operating systems with an overview of all the major aspects. The book deals with topics like computer process, operating systems and their functioning, and design. It also looks at special-purpose systems, storage management, security, distributed systems and memory.'
+        'books': books, 
     }
 
     return render(request, "main.html", context)
+
+def add_book(request):
+    form = ItemForm(request.POST or None)
+
+    if form.is_valid() and request.method == "POST":
+        form.save()
+        return HttpResponseRedirect(reverse('main:show_main'))
+    
+    context = {'form': form}
+    return render(request, "add_book.html", context)
