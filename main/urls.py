b882ca068bf9e01c1086f23d8558a43e0f35fe43
fahmi-ramadhan <fahmi122004@gmail.com>
2023-09-18 22:33:42 +0700

    Add functions to show xml and json

--- main/urls.py
+++ main/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
-from main.views import show_main, add_book
+from main.views import show_main, add_book, show_xml, show_json, show_xml_by_id, show_json_by_id
 
 app_name = 'main'
 
 urlpatterns = [
     path('', show_main, name='show_main'),
     path('add_book', add_book, name='add_book'),
+    path('xml/', show_xml, name='show_xml'),
+    path('json/', show_json, name='show_json'),
+    path('xml/<int:id>/', show_xml_by_id, name='show_xml_by_id'),
+    path('json/<int:id>/', show_json_by_id, name='show_json_by_id'),
 ]
--- main/views.py
+++ main/views.py
@@ -3,6 +3,8 @@ from django.http import HttpResponseRedirect
 from main.forms import ItemForm
 from django.urls import reverse
 from main.models import Item
+from django.http import HttpResponse
+from django.core import serializers
 
 def show_main(request):
     books = Item.objects.all()
@@ -25,3 +27,23 @@ def add_book(request):
     
     context = {'form': form}
     return render(request, "add_book.html", context)
+
+def show_html(request):
+    data = Item.objects.all()
+    return HttpResponse(serializers.serialize('html', data), content_type="application/html")
+
+def show_xml(request):
+    data = Item.objects.all()
+    return HttpResponse(serializers.serialize('xml', data), content_type="application/xml")
+
+def show_json(request):
+    data = Item.objects.all()
+    return HttpResponse(serializers.serialize('json', data), content_type="application/json")
+
+def show_xml_by_id(request, id):
+    data = Item.objects.filter(pk=id)
+    return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
+
+def show_json_by_id(request, id):
+    data = Item.objects.filter(pk=id)
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
