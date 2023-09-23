423c02b90f2f28d3e3ceeb15b7772f11a02b7cf8
fahmi-ramadhan <fahmi122004@gmail.com>
2023-09-23 21:07:36 +0700

    Revert "Update Tugas 3"
    This reverts commit a608aa48f11ac5f82aaad8fdae599fbdc15f525c.

--- main/views.py
+++ main/views.py
@@ -1,29 +1,23 @@
-from django.shortcuts import render, redirect
-from django.http import HttpResponseRedirect, HttpResponse
+from django.shortcuts import render
+from django.http import HttpResponseRedirect
 from main.forms import ItemForm
 from django.urls import reverse
 from main.models import Item
+from django.http import HttpResponse
 from django.core import serializers
-from django.contrib.auth.forms import UserCreationForm
-from django.contrib import messages  
-from django.contrib.auth import authenticate, login, logout
-from django.contrib.auth.decorators import login_required
-import datetime
 
-@login_required(login_url='/login')
 def show_main(request):
-    books = Item.objects.filter(user=request.user)
+    books = Item.objects.all()
     total_books = 0
     for book in books:
         total_books += book.amount
 
     context = {
         'app_name': 'Library Management System',
-        'student_name': request.user.username,
+        'student_name': 'Fahmi Ramadhan',
         'class': 'PBP A',
         'books': books,
         'total_books': total_books,
-        'last_login': request.COOKIES['last_login'],
     }
 
     return render(request, "main.html", context)
@@ -32,46 +26,11 @@ def add_book(request):
     form = ItemForm(request.POST or None)
 
     if form.is_valid() and request.method == "POST":
-        item = form.save(commit=False)
-        item.user = request.user
-        item.save()
+        form.save()
         return HttpResponseRedirect(reverse('main:show_main'))
     
     context = {'form': form}
     return render(request, "add_book.html", context)
-
-def register(request):
-    form = UserCreationForm()
-
-    if request.method == "POST":
-        form = UserCreationForm(request.POST)
-        if form.is_valid():
-            form.save()
-            messages.success(request, 'Your account has been successfully created!')
-            return redirect('main:login')
-    context = {'form':form}
-    return render(request, 'register.html', context)
-
-def login_user(request):
-    if request.method == 'POST':
-        username = request.POST.get('username')
-        password = request.POST.get('password')
-        user = authenticate(request, username=username, password=password)
-        if user is not None:
-            login(request, user)
-            response = HttpResponseRedirect(reverse("main:show_main")) 
-            response.set_cookie('last_login', str(datetime.datetime.now()))
-            return response
-        else:
-            messages.info(request, 'Sorry, incorrect username or password. Please try again.')
-    context = {}
-    return render(request, 'login.html', context)
-
-def logout_user(request):
-    logout(request)
-    response = HttpResponseRedirect(reverse('main:login'))
-    response.delete_cookie('last_login')
-    return response
 
 def show_xml(request):
     data = Item.objects.all()
