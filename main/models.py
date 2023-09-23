0b4a61b0ddec9798a2d700b41f16d99226502eb0
fahmi-ramadhan <fahmi122004@gmail.com>
2023-09-23 20:36:30 +0700

    Revert "Update Tugas 3"
    This reverts commit a608aa48f11ac5f82aaad8fdae599fbdc15f525c.

--- main/models.py
+++ main/models.py
@@ -1,8 +1,6 @@
 from django.db import models
-from django.contrib.auth.models import User
 
 class Item(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=255)
     author = models.CharField(max_length=255)
     category = models.CharField(max_length=255)
