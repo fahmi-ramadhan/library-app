6cde3be558c376dc3416d5056332f65966b6f6da
fahmi-ramadhan <fahmi122004@gmail.com>
2023-09-23 21:18:32 +0700

    Revert "Revert "Update Tugas 3""
    This reverts commit 423c02b90f2f28d3e3ceeb15b7772f11a02b7cf8.

--- main/models.py
+++ main/models.py
@@ -1,6 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Item(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=255)
     author = models.CharField(max_length=255)
     category = models.CharField(max_length=255)
--- main/urls.py
+++ main/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from main.views import show_main, add_book, show_xml, show_json, show_xml_by_id, show_json_by_id
+from main.views import show_main, add_book, show_xml, show_json, show_xml_by_id, show_json_by_id, register, login_user, logout_user
 
 app_name = 'main'
 
@@ -10,4 +10,7 @@ urlpatterns = [
     path('json/', show_json, name='show_json'),
     path('xml/<int:id>/', show_xml_by_id, name='show_xml_by_id'),
     path('json/<int:id>/', show_json_by_id, name='show_json_by_id'),
+    path('register/', register, name='register'),
+    path('login/', login_user, name='login'),
+    path('logout/', logout_user, name='logout'),
 ]
